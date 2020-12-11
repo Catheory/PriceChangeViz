30c2cb2ef4f8173e61ffeafe9df17ea5bd49cbc9
Sirui Sun <catheory91@gmail.com>
2020-12-11 00:43:01 -0800

    delete autocreate imports

--- client/components/charts.js
+++ client/components/charts.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
-import {labels, vehiclePrice, vegfruitPrice, medianIncome} from '../data/chartData'
 
 class Charts extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
   render() {
     return (
       <div className="chart">
-        <Line data={this.props.chartData} options={this.props.chartData.options}/>
+        <Line
+          data={this.props.chartData}
+          options={this.props.chartData.options}
+        />
       </div>
     );
   }
